83f06281d1d001fc1e52e46f4941602f26727b7a
Антон <sierra_93@mail.ru>
2022-03-23 00:25:08 +0300

    Добавил главную страницу. Настроил базовую архитектуру проекта.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageModule } from './modules/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: MainPageModule
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/core/core-urls/api-url.ts
@@ -0,0 +1,8 @@
+/**
+ * Константа путей для бэка приложения.
+ */
+export const API_URL = {
+    // apiUrl: "http://localhost:9991",
+    apiUrl: "https://leoka-elementary.online"
+    //     apiUrl: "https://leoka-elementary.xyz" 
+};
