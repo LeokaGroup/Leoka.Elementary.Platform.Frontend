e5bd803745d1539a86c138d3968e260460f90b31
Антон <sierra_93@mail.ru>
2022-04-07 01:39:09 +0300

    Добавил страницу регистрации.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ const routes: Routes = [
   {
     path: "",
     component: MainPageModule
+  },
+
+  {
+    path: "signup",
+    loadChildren: () => import('./modules/user/user.module').then(m => m.UserMainModule)
   }
 ];
 
