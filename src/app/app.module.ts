0982898ed8a883a237642e0371caaec825afe1f2
alice23456 <103214950+alice23456@users.noreply.github.com>
2022-04-07 22:38:08 +0300

    Revert "Develop"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,16 @@
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "popper.js/dist/popper.min.js";
-import "bootstrap/dist/js/bootstrap.min.js";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderModule } from './modules/header/header.component';
-import { MainPageModule } from './modules/main-page/main-page.component';
-import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MainPageModule,
-    HeaderModule
+    AppComponent
   ],
 
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ButtonModule
+    AppRoutingModule
   ],
   providers: [],
 
